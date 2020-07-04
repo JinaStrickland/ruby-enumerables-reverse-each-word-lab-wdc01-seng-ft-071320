45f12d00172ebdeb21a3ad42177389e810faa78a
Jina Strickland <jina30@gmail.com>
2020-07-04 03:44:43 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+#sentence = "Hello there stranger"
+
+def reverse_each_word(sentence)
+  separated = sentence.split(" ")
+  array = []
+  separated.collect do | word |
+  array << word.reverse
+  end
+  array.join(" ")
+end
+
+
+# def reverse_each_word(sentence)
+#   array = sentence.split(" ")
+#   array.collect { | word | word.reverse }.join(" ")
+# end
+
+
+#reverse_each_word(sentence)
